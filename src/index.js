0855346af4249b45fd47fc208942be69c0694b1f
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-02-06 16:58:32 -0300

    iniciando projeto react

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// pegando todo app criado e joga dentro da div root
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
